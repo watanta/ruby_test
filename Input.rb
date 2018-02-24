a5e132ca4d600881de269e4d59b869ae384c5609
Unknown <watanabe101watanabe@yahoo.co.jp>
2018-02-24 23:12:14 +0900

    ex1,ex2できた

---
+++ Input.rb
@@ -0,0 +1,16 @@
+up = 0
+down = 0
+right= 0
+left = 0
+puts " 文字を入力してください"
+input = gets.chomp!
+input.each_char {|ch| if ch == "R" then right +=1 
+                        elsif ch == "L" then left += 1
+                        elsif ch == "U" then up +=1
+                        elsif ch == "D" then down +=1 end}
+x = right - left
+y = up - down
+
+puts ""
+puts input
+puts "("+x.to_s+","+y.to_s+")"
